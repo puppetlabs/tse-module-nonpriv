d8990b6f39a9e6126eb3bfb2b01dde76fb55b3e1
jpadams <jeremy@puppetlabs.com>
2014-02-10 20:03:37 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,24 @@
-class nonpriv {}
+class nonpriv ($user, $password, $server) {
+
+  class { 'nonpriv::user_created_by_admin':
+    nonpriv_user      => $user,
+    password          => $password,
+    ensure            => present,
+    certname          => $user,
+    server            => $server,
+    enable_sched_task => true,
+  }
+
+  class { 'nonpriv::puppet_run_sched_by_user':
+    nonpriv_user      => $user,
+    password          => $password,
+    ensure            =>'present',
+    certname          =>$user,
+    server            =>$server,
+    run_interval_mins =>'30',
+  }
+
+  Class[ 'nonpriv::user_created_by_admin' ] ->
+  Class[ 'nonpriv::puppet_run_sched_by_user' ]
+
+}
