0e3d127db8de594dd4ca7f1ac478f5e4c0fb73ac
Jeremy Adams <jeremy@puppetlabs.com>
2014-09-29 12:54:48 -0700

    fixing fact

--- lib/facter/isadmin.rb
+++ lib/facter/isadmin.rb
@@ -4,10 +4,14 @@ Facter.add(:isadmin) do
 
   setcode do
     ps = Tempfile.new('isadmin_fact_powershell_script')
-    ps.write("$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent([Security.Principal.TokenAccessLevels]'Query,Duplicate'))
-              $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
-              return $isAdmin")
+    script = <<END
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent([Security.Principal.TokenAccessLevels]'Query,Duplicate'))
+    $isAdmin = $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    return $isAdmin
+END
+    ps.write(script)
     command = "powershell -command #{ps.path}"
     Facter::Core::Execution.exec(command)
+    ps.close
   end
 end
