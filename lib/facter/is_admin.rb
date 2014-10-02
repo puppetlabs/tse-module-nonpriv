a665e0e01cd3996b12634b6ef387dac06aafc8fa
jpadams <jeremy@puppetlabs.com>
2014-10-02 12:09:13 -0700

    remove newlines after call to id

--- lib/facter/is_admin.rb
+++ lib/facter/is_admin.rb
@@ -21,14 +21,14 @@ Facter.add(:is_admin) do
   confine do
     Facter::Core::Execution.which('id') && Facter.value(:kernel) != "SunOS"
   end
-  setcode { (Facter::Core::Execution.exec('id -u') == 0).to_s }
+  setcode { (Facter::Core::Execution.exec('id -u').chomp == 0).to_s }
 end
 
 Facter.add(:is_admin) do
   confine :kernel => :SunOS
   setcode do
     if File.exist? '/usr/xpg4/bin/id'
-      (Facter::Core::Execution.exec('/usr/xpg4/bin/id -u') == 0).to_s
+      (Facter::Core::Execution.exec('/usr/xpg4/bin/id -u').chomp == 0).to_s
     end
   end
 end
