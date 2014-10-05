e46f28d7f41d7bb657ceb2f2db371d8c6f4420e7
Jeremy Adams <jeremy@puppetlabs.com>
2014-10-05 11:06:38 -0700

    change to clientcert-based name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class nonpriv ($suffix='nonadmin', $password='puppetlabs', $server='puppet') {
   if $is_admin {
     $kern = downcase($kernel) 
-    $host = regsubst($hostname, '[.]', '_', 'G')
+    $cert = values_at(split($clientcert, '[.]'), 0)
 
-    nonpriv::user_created_by_admin { "${kern}_${suffix}_${host}":
+    nonpriv::user_created_by_admin { "${kern}_${suffix}_${cert}":
       ensure   => present,
       password => $password,
       server   => $server,
