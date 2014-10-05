ec5987192b635bb80c2afe5f46207643a4e54bba
Jeremy Adams <jeremy@puppetlabs.com>
2014-10-04 23:57:12 -0700

    change to regsubst

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,9 @@
 class nonpriv ($suffix='nonadmin', $password='puppetlabs', $server='puppet') {
   if $is_admin {
     $kern = downcase($kernel) 
-    $cert = regsubst($clientcert, '[.]', '_', 'G')
+    $host = regsubst($hostname, '[.]', '_', 'G')
 
-    nonpriv::user_created_by_admin { "${kern}_${suffix}_${cert}":
+    nonpriv::user_created_by_admin { "${kern}_${suffix}_${host}":
       ensure   => present,
       password => $password,
       server   => $server,
