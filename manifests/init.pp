cfa90f4a801273ba1da27b813e9cb6e06ad0e82f
Jeremy Adams <jeremy@puppetlabs.com>
2014-10-04 22:52:33 -0700

    comment and fixup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,1 +1,11 @@
-class nonpriv {}
+class nonpriv ($suffix='nonadmin', $password='puppetlabs', $server='puppet') {
+  if $is_admin {
+    $kern = downcase($kernel)
+
+    nonpriv::user_created_by_admin { "${kern}_${suffix}":
+      ensure   => present,
+      password => ${password},
+      server   => ${server},
+    }
+  }
+}
